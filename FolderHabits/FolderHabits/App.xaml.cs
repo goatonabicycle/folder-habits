2eac9600883a26898692bca61e0fce3cf244b0e2
Rowan <malice.peanutbutter@gmail.com>
2025-03-10 00:42:34 +0200

    Initial commit

---
+++ FolderHabits/FolderHabits/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FolderHabits;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ FolderHabits/FolderHabits/Models/HabitConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FolderHabits.Models
+{
+    public class HabitConfiguration
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; } = string.Empty;
+        public string FolderPath { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public bool IsActive { get; set; } = true;
+        public List<string> FileExtensionsToTrack { get; set; } = new List<string>();
+    }
+}
